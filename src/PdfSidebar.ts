386ca1c8ceee1617aa3da2f5f42a12e9574585e0
Isidora Lina <dev@lina.moe>
2024-02-27 14:47:46 +0000

    Replace sidebar with text if no focused pdf
    imo, being able to change stuff about the PDF you can't even see could be counterproductive.

--- src/PdfSidebar.ts
+++ src/PdfSidebar.ts
@@ -1,5 +1,8 @@
 import { ItemView } from "obsidian";
 import { PdfPreviewView } from "./PdfPreviewView";
+import { El } from "./util";
+
+import "./PdfSidebar.css";
 
 export default class PdfSidebar extends ItemView {
     static VIEW_TYPE = "pdf-preview-sidebar";
@@ -22,17 +25,28 @@ export default class PdfSidebar extends ItemView {
         this.registerEvent(this.app.workspace.on('active-leaf-change', leaf => {
             console.log('active-leaf-change', leaf);
 
-            if (leaf?.view instanceof PdfPreviewView)
+            if (leaf?.view instanceof PdfPreviewView) {
                 this.viewChanged(leaf.view);
-            
-            if (leaf?.view === this)
-                console.log(`we're focused baby!`);
+                this.show();
+            } else if (leaf?.view !== this) this.hide();
         }));
+
+        this.contentEl.addClass('pp-sidebar');
+
+        El(this.containerEl, `div.if-invalid-view`, el => {
+            El(el, `div.pane-empty`, { text: 'No PDF Preview focused' });
+        });
     }
 
     viewChanged(view: PdfPreviewView) {
-        console.log('got a PdfPreviewView!');
-
         this.pdfView = view;
+    }
+
+    show() {
+        this.contentEl.removeClass('invalid-view');
+    }
+
+    hide() {
+        this.contentEl.addClass('invalid-view');
     }
 }
