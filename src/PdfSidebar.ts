8411e9aa3eff919f928921f1c9dcd7e8fdfa6ab2
Isidora Lina <dev@lina.moe>
2024-02-27 19:56:16 +0000

    Add more sidebar functionality; fix view actions hiding
    Made it so that the view actions only hide after the sidebar has been focused, because otherwise it'd bug out a *lot* when switching random views.

--- src/PdfSidebar.ts
+++ src/PdfSidebar.ts
@@ -1,4 +1,4 @@
-import { ItemView } from "obsidian";
+import { ItemView, WorkspaceLeaf, EventRef } from "obsidian";
 import { PdfPreviewView } from "./PdfPreviewView";
 import { El, Icon } from "./util";
 
@@ -8,6 +8,27 @@ export default class PdfSidebar extends ItemView {
     static VIEW_TYPE = "pdf-preview-sidebar";
 
     pdfView?: PdfPreviewView | null = null;
+
+    pageNumberEl: HTMLInputElement;
+    maxPagesEl: HTMLElement;
+
+    leafChangeRef: EventRef;
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+
+        this.registerEvent(this.leafChangeRef = this.app.workspace.on('active-leaf-change', leaf => {
+            console.log('active-leaf-change', leaf);
+
+            if (leaf?.view instanceof PdfPreviewView) {
+                this.viewChanged(leaf.view);
+                this.show();
+            } else if (leaf?.view !== this) this.hide();
+            else {
+                this.pdfView?.containerEl.addClass("has-sidebar");
+            }
+        }));
+    }
 
     getViewType(): string {
         return PdfSidebar.VIEW_TYPE;
@@ -22,15 +43,6 @@ export default class PdfSidebar extends ItemView {
     }
 
     async onOpen() {
-        this.registerEvent(this.app.workspace.on('active-leaf-change', leaf => {
-            console.log('active-leaf-change', leaf);
-
-            if (leaf?.view instanceof PdfPreviewView) {
-                this.viewChanged(leaf.view);
-                this.show();
-            } else if (leaf?.view !== this) this.hide();
-        }));
-
         this.contentEl.addClass('pp-sidebar');
 
         El(this.containerEl, `div.if-invalid-view`, el => {
@@ -41,11 +53,40 @@ export default class PdfSidebar extends ItemView {
             Icon(el, 'refresh-cw', this.refreshPreview.bind(this));
             Icon(el, 'save', this.savePdf.bind(this));
         }), this.contentEl);
+
+        this.containerEl.insertBefore(El(null, `div.pp-sidebar.icons-container`, el => {
+            Icon(el, 'arrow-left', this.prevPage.bind(this));
+
+            // todo: somehow don't copy this code from PdfPreviewView
+            El(el, 'input.pdf-page-input', { attr: {
+               type: "number" 
+            }, cb: input => {
+                this.pageNumberEl = input;
+                input.addEventListener("change", ev => {
+                    if (!this.pdfView) return;
+                    if (!this.pdfView.setPageNumber(input.valueAsNumber)) {
+                        ev.preventDefault();
+                        input.value = this.pdfView.pageNumber.toString();
+                    }
+                })
+            } });
+
+            El(el, `span.pdf-page-numbers`, span => this.maxPagesEl = span);
+
+            Icon(el, 'arrow-right', this.nextPage.bind(this));
+        }), this.contentEl);
+    }
+
+    async onClose() {
+        this.pdfView?.containerEl.removeClass("has-sidebar");
+        this.pdfView = null;
+
+        this.app.workspace.offref(this.leafChangeRef);
     }
 
     viewChanged(view: PdfPreviewView) {
         this.pdfView = view;
-        view.containerEl.addClass("has-sidebar");
+        // view.containerEl.addClass("has-sidebar");
     }
 
     show() {
@@ -64,5 +105,16 @@ export default class PdfSidebar extends ItemView {
 
     savePdf() {
         this.pdfView?.save();
+    }
+
+    // todo: somehow reduce the copying of code here too
+    prevPage() {
+        if (this.pdfView?.setPageNumber(this.pdfView.pageNumber - 1))
+            this.pageNumberEl.value = this.pdfView.pageNumber.toString();
+    }
+
+    nextPage() {
+        if (this.pdfView?.setPageNumber(this.pdfView.pageNumber + 1))
+            this.pageNumberEl.value = this.pdfView.pageNumber.toString();
     }
 }
